401a1feef2dc45120c1b9ed738b7d13661a9edd0
EasternCatNZL <brianpwwong@gmail.com>
2017-08-06 21:21:06 +1200

    mouse pos screen to world, only effects floor. for targeting

---
+++ ActionRPG/Assets/Scripts/MouseControl/MouseTarget.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTarget : MonoBehaviour {
+
+    //debug thing
+    public GameObject thing;
+
+    //transform for other scripts
+    [HideInInspector]
+    public Transform mouseTargetPos;
+
+    //raycast stuff
+    public float rayLength; //length of ray
+    public LayerMask myMask; //layermask for raycast
+
+    private RaycastHit rayHit; //ray hit info
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        GetWorldMousePos();
+	}
+
+    //return the position in world floor surface that the mouse is at
+    void GetWorldMousePos()
+    {
+        //send a ray from the position of mouse on screen
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out rayHit, rayLength, myMask))
+        {
+            GameObject clone = thing;
+            Instantiate(clone, rayHit.point, Quaternion.identity);
+        }
+    }
+}
