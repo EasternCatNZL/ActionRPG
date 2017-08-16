c0161fc6e497b6e6ed39cc3bf92ee4892080c56a
EasternCatNZL <brianpwwong@gmail.com>
2017-08-16 16:54:59 +1200

    Town scene now gets current progression of town, and then spawns in a town when the scene loads

--- ActionRPG/Assets/Scripts/Gameloop/TownProgression.cs
+++ ActionRPG/Assets/Scripts/Gameloop/TownProgression.cs
@@ -8,7 +8,9 @@ public class TownProgression : MonoBehaviour {
     [Tooltip("Starting town object")]
     public GameObject startTownObject;
     [Tooltip("Second town object")]
-    public GameObject secondTownObject;
+    public GameObject midTownObject;
+    [Tooltip("Third town object")]
+    public GameObject largeTownObject;
 
     [Header("Character objects")]
     [Tooltip("Player object")]
@@ -18,15 +20,15 @@ public class TownProgression : MonoBehaviour {
     private enum TownCurrentState
     {
         Starting,
-        Progressed
+        Progressed,
+        Complete
     }
 
     private TownCurrentState currentState = TownCurrentState.Starting; //current state of the town
 
 	// Use this for initialization
 	void Start () {
-        //dont destroy
-        DontDestroyOnLoad(gameObject);
+        
     }
 	
 	// Update is called once per frame
@@ -35,20 +37,25 @@ public class TownProgression : MonoBehaviour {
 	}
 
     //load the town based on current progression
-    private void LoadTown()
+    public void LoadTown(Transform location)
     {
         //make clone object
         GameObject townClone = startTownObject;
+        //get town object based on current state of town
         if (currentState == TownCurrentState.Starting)
         {
             townClone = startTownObject;
         }
         else if (currentState == TownCurrentState.Progressed)
         {
-            townClone = secondTownObject;
+            townClone = midTownObject;
+        }
+        else if (currentState == TownCurrentState.Complete)
+        {
+            townClone = largeTownObject;
         }
         //spawn the town
-        Instantiate(townClone, Vector3.zero, Quaternion.identity);
+        Instantiate(townClone, location.position, Quaternion.identity);
         //spawn the player in
         LoadPlayer(townClone.GetComponent<TownBehaviour>().GetTownEntrySpawn());
     }
